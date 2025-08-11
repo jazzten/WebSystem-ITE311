65c338a33031a8f27d34e28041f425d83b8d2724
jazzten <Justinebrian0909@gmail.com>
2025-08-12 00:00:43 +0800

    Added Bootstrap template and Home controller

--- app/Views/home_view.php
+++ app/Views/home_view.php
@@ -1,5 +1,4 @@
-<h1 class="display-4">Welcome to ITE311 Web System</h1>
-<p class="lead">This is My homepage after using <a href="https://codeigniter.com/">CodeIgniter</a> and <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
-
+<h1>Welcome to ITE311 Web System</h1>
+<p>This is My homepage after using <a href="https://codeigniter.com/">CodeIgniter</a> and <a href="https://getbootstrap.com/">Bootstrap</a>.</p>
 
 
