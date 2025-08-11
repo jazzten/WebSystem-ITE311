64bab4b4167d0ee9ff8dc098512aa9ea82061281
Jems Crister22 <Cristerjeymss22@gmail.com>
2025-08-12 00:00:43 +0800

    Added Bootstrap template and Home controller

--- app/Views/template.php
+++ app/Views/template.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>My CI4 App</title>
+    <title>CI4 App</title>
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
